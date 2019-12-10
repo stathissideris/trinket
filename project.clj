fded587e89bf4227f193199420c42d7320bfc2bb
Stathis Sideris <stathis@pixelated-noise.com>
2019-12-10 20:29:22 +0200

    Update deps and remove project.clj

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject trinket "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:dev {:source-paths ["src" "test" "dev"]
-                   ;;:java-opts    ["-agentpath:/Applications/YourKit-Java-Profiler-2018.04.app/Contents/Resources/bin/mac/libyjpagent.jnilib"]
-                   }})
